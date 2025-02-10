3ff333dd270787eff0793b278cbb19e8e969c125
Nima Rostami <nima29893@gmail.com>
2025-02-10 12:07:38 +0330

    adding some style and stuff.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/")
 def hello_world():
-    return "helloooo nima"
+    return render_template('home.html')
 
     
 
